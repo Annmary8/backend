e506cf62acf341b579b0134fa1ab754c49250cd9
Anmmary8 <annmarybenny985@gmail.com>
2025-07-26 22:52:05 +0530

    third

---
+++ api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from api.models import User
+
+class RegisterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['email','username','first_name','last_name' ,'password']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+class LoginSerializer(serializers.Serializer):
+    username= serializers.CharField()
+    password = serializers.CharField(write_only=True)
--- api/views.py
+++ api/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from api.serializers import RegisterSerializer, LoginSerializer
+from rest_framework import status
+from django.contrib.auth import authenticate
+from api.models import User
+
+
+
+
+class RegisterView(APIView):
+
+    def post(self, request):
+        serializer = RegisterSerializer(data=request.data)    
+
+        if serializer.is_valid():
+            User.objects.create_user(**serializer.validated_data)
+            return Response({"message": "User registered successfully"}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(username=serializer.validated_data['username'], password=serializer.validated_data['password'])
+            if user:
+                return Response({"message": "Login successful"})
+            return Response({"message": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
